6a1fbed8facb5c5cbce4f690d7f06e776f26414c
Muhammed Hashir <hashiraachi818@gmail.com>
2022-10-04 19:47:44 +0530

    create new bannerbottom components then start the work

---
+++ src/Components/BannerBottom/BannerBottom.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ICON1 from "../../Assets/icon1.png";
+import ICON2 from "../../Assets/icon2.png";
+import PERSON from "../../Assets/person.png";
+import './BannerBottom.css'
+
+const BannerBottom = () => {
+  return (
+    <div>BannerBottom</div>
+  )
+}
+
+export default BannerBottom
