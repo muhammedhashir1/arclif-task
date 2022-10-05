f3dd0ba8f1ea369a41582d999e17105abc32409b
Muhammed Hashir <hashiraachi818@gmail.com>
2022-10-05 13:57:10 +0530

    footer component added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Banner from './Components/Banner/Banner';
 import BannerBottom from './Components/BannerBottom/BannerBottom';
 import Cards from './Components/Cards/Cards';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Banner/>
       <BannerBottom/>
       <Cards/>
+      <Footer/>
     </div>
   );
 }
